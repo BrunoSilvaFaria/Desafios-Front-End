acb62b6f1cc0ad8ed2599cfac9e97bf952cc7af7
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-05-31 11:20:33 -0300

    update: 31-05

--- FrontEndTutoriais/responsiveNavBar/main.js
+++
@@ -1,6 +0,0 @@
-const icon = document.querySelector(".toggle-button");
-const menu = document.querySelector(".link__conteiner");
-
-icon.addEventListener('click', function () {
-    menu.classList.toggle('active')
-});
