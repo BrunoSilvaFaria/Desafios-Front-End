9954ff3a756f5c0946f2d05ed3d5786b09810475
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-06-23 12:38:05 -0300

    feat: adição de scripts js, com finalidade de gerar cards automaticos

---
+++ FrontEndMenthor/Junior/notifications-page-main/js/data.js
@@ -0,0 +1,67 @@
+export default [ 
+    {
+        img: "../assets/images/avatar-mark-webber.webp",
+        name: "Mark Webber",
+        notification: "reacted to your recent post",
+        content: "My first tournament today!",
+        time: "1m",
+        mensage: "",
+    },
+
+    {
+        img: "../assets/images/avatar-angela-gray.webp",
+        name: "Angela Gray",
+        notification: "followed you",
+        content: "",
+        time: "5m",
+        mensage: "",
+    },
+
+    {
+        img: "../assets/images/avatar-jacob-thompson.webp",
+        name: "Jacob Thompson",
+        notification: "has joined your group",
+        content: "Chess Club",
+        time: "1 day",
+        mensage: "",
+    },
+    
+    {
+        img: "../assets/images/avatar-rizky-hasanuddin.webp",
+        name: "Rizky Hasanuddin",
+        notification: "sent you a private message",
+        content: "",
+        time: "5 days",
+        mensage: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+    },
+
+    {
+        img: "../assets/images/avatar-kimberly-smith.webp",
+        name: "Kimberly Smith",
+        notification: "commented on your picture",
+        content: "",
+        time: "1 week",
+        mensage: "",
+    },
+   
+    {
+        img: "../assets/images/avatar-nathan-peterson.webp",
+        name: "Nathan Peterson",
+        notification: "reacted to your recent post",
+        content: "5 end-game strategies to increase your win rate",
+        time: "2 weeks",
+        mensage: "",
+    },
+
+    {
+        img: "../assets/images/avatar-anna-kim.webp",
+        name: "Anna Kim ",
+        notification: "left the group",
+        content: "Chess Club",
+        time: "2 weeks",
+        mensage: "",
+    }
+]
+    
+   
+
