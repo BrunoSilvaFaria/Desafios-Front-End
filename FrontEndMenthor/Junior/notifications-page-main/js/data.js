1f39f40f703df532dc0fa0f933b17705bdc6d516
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-06-23 17:03:19 -0300

    feat: adição de chaves e ordenação

--- FrontEndMenthor/Junior/notifications-page-main/js/data.js
+++ FrontEndMenthor/Junior/notifications-page-main/js/data.js
@@ -1,65 +1,79 @@
 export default [ 
     {
+        content: "My first tournament today!",
+        group: '',
         img: "../assets/images/avatar-mark-webber.webp",
+        mensage: "",
         name: "Mark Webber",
         notification: "reacted to your recent post",
-        content: "My first tournament today!",
-        time: "1m",
-        mensage: "",
+        read: false,
+        time: "1m"
     },
 
     {
+        content: "",
+        group: '',
         img: "../assets/images/avatar-angela-gray.webp",
+        mensage: "",
         name: "Angela Gray",
         notification: "followed you",
-        content: "",
-        time: "5m",
-        mensage: "",
+        read: false,
+        time: "5m"
     },
 
     {
+        content: "",
+        group: "Chess Club",
         img: "../assets/images/avatar-jacob-thompson.webp",
+        mensage: "",
         name: "Jacob Thompson",
         notification: "has joined your group",
-        content: "Chess Club",
-        time: "1 day",
-        mensage: "",
+        read: false,
+        time: "1 day"
     },
     
     {
+        content: "",
+        group: '',
         img: "../assets/images/avatar-rizky-hasanuddin.webp",
+        mensage: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
         name: "Rizky Hasanuddin",
         notification: "sent you a private message",
-        content: "",
-        time: "5 days",
-        mensage: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+        read: true,
+        time: "5 days"
     },
 
     {
+        content: "",
+        group: '',
         img: "../assets/images/avatar-kimberly-smith.webp",
+        mensage: "",
         name: "Kimberly Smith",
         notification: "commented on your picture",
-        content: "",
-        time: "1 week",
-        mensage: "",
+        read: true,
+        time: "1 week"
     },
    
     {
+        content: "5 end-game strategies to increase your win rate",
+        group: '',
         img: "../assets/images/avatar-nathan-peterson.webp",
+        mensage: "",
         name: "Nathan Peterson",
         notification: "reacted to your recent post",
-        content: "5 end-game strategies to increase your win rate",
-        time: "2 weeks",
-        mensage: "",
+        read: true,
+        time: "2 weeks"
     },
 
     {
+        content: "",
+        group: "Chess Club",
         img: "../assets/images/avatar-anna-kim.webp",
+        mensage: "",
         name: "Anna Kim ",
         notification: "left the group",
-        content: "Chess Club",
-        time: "2 weeks",
-        mensage: "",
+        read: true,
+        time: "2 weeks"
     }
 ]
     
