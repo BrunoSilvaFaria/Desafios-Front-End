1d782112a77cbc962a749f88585d69cec50fcaf7
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-06-23 17:06:57 -0300

    feat: adição de chave, representando comentários

--- FrontEndMenthor/Junior/notifications-page-main/js/data.js
+++ FrontEndMenthor/Junior/notifications-page-main/js/data.js
@@ -1,22 +1,24 @@
 export default [ 
     {
         content: "My first tournament today!",
-        group: '',
+        group: "",
         img: "../assets/images/avatar-mark-webber.webp",
         mensage: "",
         name: "Mark Webber",
         notification: "reacted to your recent post",
+        coment: "",
         read: false,
         time: "1m"
     },
 
     {
         content: "",
-        group: '',
+        group: "",
         img: "../assets/images/avatar-angela-gray.webp",
         mensage: "",
         name: "Angela Gray",
         notification: "followed you",
+        coment: "",
         read: false,
         time: "5m"
     },
@@ -28,39 +30,43 @@ export default [
         mensage: "",
         name: "Jacob Thompson",
         notification: "has joined your group",
+        coment: "",
         read: false,
         time: "1 day"
     },
     
     {
         content: "",
-        group: '',
+        group: "",
         img: "../assets/images/avatar-rizky-hasanuddin.webp",
         mensage: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
         name: "Rizky Hasanuddin",
         notification: "sent you a private message",
+        coment: "",
         read: true,
         time: "5 days"
     },
 
     {
         content: "",
-        group: '',
+        group: "",
         img: "../assets/images/avatar-kimberly-smith.webp",
         mensage: "",
         name: "Kimberly Smith",
         notification: "commented on your picture",
+        coment: '../assets/images/image-chess.webp',
         read: true,
         time: "1 week"
     },
    
     {
         content: "5 end-game strategies to increase your win rate",
-        group: '',
+        group: "",
         img: "../assets/images/avatar-nathan-peterson.webp",
         mensage: "",
         name: "Nathan Peterson",
         notification: "reacted to your recent post",
+        coment: "",
         read: true,
         time: "2 weeks"
     },
@@ -72,6 +78,7 @@ export default [
         mensage: "",
         name: "Anna Kim ",
         notification: "left the group",
+        coment: "",
         read: true,
         time: "2 weeks"
     }
